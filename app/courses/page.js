95b31d3a2b76581a9025754ea4062fe03d13a518
Usama Ansari <student9970@gmail.com>
2024-09-20 01:52:19 +0530

    Design change of the courses page

--- app/courses/page.js
+++ app/courses/page.js
@@ -1,5 +1,5 @@
 "use client";
-import MiniCourseCard from "@/components/MiniCourseCard";
+import MegaCourseContent from "@/components/MegaCourseContent";
 import styles from "./page.module.css";
 import Footer from "@/components/Footer";
 import Link from "next/link";
@@ -36,14 +36,24 @@ const page = () => {
         <Image src={classes} />
       </div>
       <div className={styles.content}>
+        <p className={styles.coursesIntro}>
+          At MS Tech Campus, we offer a wide range of in-demand tech courses
+          designed to help you master the skills needed in today’s fast-paced
+          technology landscape. Whether you’re a beginner or an experienced
+          professional, our courses are tailored to fit your needs and help you
+          excel in your career.
+        </p>
         <div className={styles.cardsContainer}>
           {data ? (
             data.map((course) => (
-              <MiniCourseCard
+              <MegaCourseContent
                 courseLink={`/courses/course/${course._id}`}
                 key={course._id}
+                tag={course.tag}
                 title={course.title}
                 imgSrc={course.imgSrc}
+                description={course.description}
+                details={course.details}
               />
             ))
           ) : (
